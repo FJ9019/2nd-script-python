21a6c8d04d6aa8b068e5d7492f24945de63d0144
FJ9019 <feenlus24@gmail.com>
2024-12-16 12:07:20 -0500

    Update 2.0

--- pendu_game.py
+++ pendu_game.py
@@ -26,9 +26,20 @@ for i in range(mot_len):
 while loop:
     print(mystere)
     mot_utilisateur = input(f"Quel est le mot caché !")
-
-    if mot_utilisateur != mot:
-        print(f"Désolé le mot entré ne correspond pas !")
-    else:
-        print(f"Bravo le mot entré est bien : {mot} !")
-        loop = False
+    
+    if not len(mot_utilisateur) == mot_len:
+        print(f"Déslé vous devez entré un mot contenant {mot_len} caractères !")
+    
+    else:    
+        if mot_utilisateur != mot:
+            mystere_backup = mystere
+            mystere = ""
+            for i in range(mot_len):
+                if mot_utilisateur[i] == mot[i]:
+                    mystere += mot[i]
+                else:
+                    mystere += mystere_backup[i]    
+            print(f"Désolé le mot entré ne correspond pas !")
+        else:
+            print(f"Bravo le mot entré est bien : {mot} !")
+            loop = False
