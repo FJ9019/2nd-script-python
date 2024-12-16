db51327e10cff13083c7518a26dfa7289cf37f52
FJ9019 <feenlus24@gmail.com>
2024-12-16 11:51:09 -0500

    Update 1.0

--- pendu_game.py
+++ pendu_game.py
@@ -10,5 +10,25 @@ dictionnaire = [
 ]
 
 mot = dictionnaire[3]
+mot_len = len(mot)
+mystere = ""
 
-print(mot)
+loop = True
+
+for i in range(mot_len):
+    if i%2 == 0:
+        mystere += mot[i]
+    else:
+        mystere += "*"
+     
+
+
+while loop:
+    print(mystere)
+    mot_utilisateur = input(f"Quel est le mot caché !")
+
+    if mot_utilisateur != mot:
+        print(f"Désolé le mot entré ne correspond pas !")
+    else:
+        print(f"Bravo le mot entré est bien : {mot} !")
+        loop = False
