87141419eecdd13b1606599cb9fc62ec4ffce8b3
FJ9019 <feenlus24@gmail.com>
2024-12-19 14:09:00 -0500

    Update Pendu_Game 2.0

--- pendu_game 2.0.py
+++ pendu_game 2.0.py
@@ -14,6 +14,29 @@ def generate_mystery_word(dictionnaire):
         else:
             mystere += "*"
             
-    return mot, mystere        
+    return mot, mystere
+
+def check_it(mystere, mot, user_try):
+    status = False
+    msg = None
+    if not len(user_try) == len(mot):
+        msg = f"Déslé vous devez entré un mot contenant {len(mot)} caractères !"
+        return status, msg, mystere
+    
+    if mot_utilisateur != mot:
+        mystere_backup = mystere
+        mystere = ""
+        for i in range(mot_len):
+            if mot_utilisateur[i] == mot[i]:
+                if i>0:
+                    if mot[i-1] ==mot_utilisateur[i-1]:
+                        mystere += mot[i]
+                    else:
+                        mystere += mystere_backup[i]
+                else:
+                    mystere += mystere_backup[i]
+            else:
+                mystere += mystere_backup[i]
+        print(f"Désolé le mot entré ne correspond pas !")                            
     
 print(generate_mystery_word(["secret", "cacher", "mystere"]))
